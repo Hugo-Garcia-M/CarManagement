1f98711c49295fdb47e403313102ce2b252487ba
Víctor Hugo García Moreno <victorhugogarciamoreno2@gmail.com>
2022-02-27 17:11:53 -0600

    Inicio del proyecto

---
+++ app/src/main/java/com/example/carmanchmen/Salida_Carros.java
@@ -0,0 +1,14 @@
+package com.example.carmanchmen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Salida_Carros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salida_carros);
+    }
+}
